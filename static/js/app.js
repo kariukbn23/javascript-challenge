ba31fe916321f73b0ea3de3b606f1aa444f29380
kariukbn23 <54543371+kariukbn23@users.noreply.github.com>
2020-05-12 14:08:44 -0500

    Update

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,23 @@ var tableData = data;
 var tbody = d3.select("tbody");
 console.log("Hello & Welcome to BK's Code")
 
+// We first need to build a function that captures our data into an html table 
+
+function displayData(data){ 
+    tbody.html("");
+
+//     // Next create loop that goes through data to capture specific data points we need
+
+//     data.forEach(function(sighting){
+    
+//     // The captured values then need to added to a new tree of data
+
+//     new_tree = tbody.append("tr")
+
+//     // We can use object enteries now to get specific key/value pairs from the data dictionary
+
+//     Object.entries(sighting).forEach(function([key, value]){
+//         new_tabledata = new_tree.append("td").text(value)	
+//     })
+// })}
+
